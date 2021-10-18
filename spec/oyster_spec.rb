9e5b482e3d401ed8c6bc8d717afda9c86f6ffc7d
NenehB <Neneh.Bojang@digital.justice.gov.uk>
2021-10-18 14:45:53 +0100

    Refactor test

--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -2,10 +2,13 @@ require 'oyster'
 describe Oyster do 
 
   it 'initialize balance' do 
+    subject = Oyster.new
     expect(subject.balance).to eq (INITIAL_BALANCE)
   end 
   it 'should add money to card'do 
+    subject = Oyster.new
     subject.add_balance(20)
     expect(subject.balance).to eq 20
   end 
+  
 end 
